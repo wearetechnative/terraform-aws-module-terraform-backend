3839ec8643f2d0784335a4690f0890a73462a05f
Ronald <ronald@technative.nl>
2023-03-05 14:25:03 +0100

    TLDA-309: Fix missing KMS key argument.

--- dynamodb.tf
+++ dynamodb.tf
@@ -2,6 +2,7 @@ module "state_lock" {
   source   = "git@github.com:TechNative-B-V/terraform-aws-module-dynamodb.git/?ref=2016bbf331807b9832e3e9bc01ecd292bfefe5d3"
 
   name           = "terraform-state-lock${local.name}"
+  kms_key_arn = var.kms_key_arn
   partition_key = "LockID"
   partition_key_type = "S"
 }
