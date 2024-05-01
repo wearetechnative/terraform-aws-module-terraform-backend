ccc0353785d913a59018d3d6fb3dd9d64aefef28
Andrew Ng <andrew@technative.eu>
2024-05-01 10:26:31 +0200

    Added use fix name variable to trigger prefix or not

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 module "backend" {
-  source   = "git@github.com:wearetechnative/terraform-aws-s3.git/?ref=41f66d2b2357c9cb0c64488eb92812c10d224823"
+  source = "git@github.com:wearetechnative/terraform-aws-s3.git/?ref=41f66d2b2357c9cb0c64488eb92812c10d224823"
 
-  name        = lower("terraform-state${local.name}")
-  use_fixed_name = true
-  kms_key_arn = var.kms_key_arn
+  name           = lower("terraform-state${local.name}")
+  use_fixed_name = var.use_fixed_name
+  kms_key_arn    = var.kms_key_arn
 }
