25dde4d14a8c2ce1a874fc38f357fabe44948676
Caspersonn <lucakasper8@gmail.com>
2025-10-23 13:53:30 +0200

    Ref update

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,5 +1,5 @@
 module "state_lock" {
-  source = "github.com/wearetechnative/terraform-aws-module-dynamodb.git/?ref=391776786f83458afa156503fe7e70ab1019f514"
+  source = "github.com/wearetechnative/terraform-aws-module-dynamodb.git?ref=391776786f83458afa156503fe7e70ab1019f514"
 
   name               = var.add_name_prefix_to_dynamodb == true ? "terraform-state-lock${local.name}" : "terraform-state-lock"
   kms_key_arn        = var.kms_key_arn
--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "backend" {
-  source = "github.com/wearetechnative/terraform-aws-s3.git/?ref=945d79d5d12cbc2e281afae53c74367a3c6bfd6e"
+  source = "github.com/wearetechnative/terraform-aws-s3.git?ref=945d79d5d12cbc2e281afae53c74367a3c6bfd6e"
 
   name           = lower("terraform-state${local.name}")
   use_fixed_name = var.use_fixed_name
